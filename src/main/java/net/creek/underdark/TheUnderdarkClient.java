f27f1f3a473cec53ec4f3ed1df9d81e9b89779ec
Spidereye <harrisonzrede@gmail.com>
2026-01-29 23:11:55 -0600

    Initial commit

---
+++ src/main/java/net/creek/underdark/TheUnderdarkClient.java
@@ -0,0 +1,10 @@
+package net.creek.underdark;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheUnderdarkClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/creek/underdark/TheUnderdarkDataGenerator.java
@@ -0,0 +1,11 @@
+package net.creek.underdark;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TheUnderdarkDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
