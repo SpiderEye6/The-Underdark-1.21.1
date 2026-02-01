ac448dc6738ccd8ac3b0481bac69afbdb0bb7393
Spidereye <harrisonzrede@gmail.com>
2026-02-01 17:41:10 -0600

    Worldgen Package Implemented

--- src/main/java/net/creek/underdark/TheUnderdarkDataGenerator.java
+++ src/main/java/net/creek/underdark/TheUnderdarkDataGenerator.java
@@ -1,8 +1,12 @@
 package net.creek.underdark;
 
 import net.creek.underdark.datagen.*;
+import net.creek.underdark.world.ModConfiguredFeatures;
+import net.creek.underdark.world.ModPlacedFeatures;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.registry.RegistryBuilder;
+import net.minecraft.registry.RegistryKeys;
 
 public class TheUnderdarkDataGenerator implements DataGeneratorEntrypoint {
 	@Override
@@ -15,5 +19,11 @@ public class TheUnderdarkDataGenerator implements DataGeneratorEntrypoint {
 		pack.addProvider(ModModelProvider::new);
 		pack.addProvider(ModRecipeProvider::new);
 		pack.addProvider(ModRegistryDataGenerator::new);
+	}
+
+	@Override
+	public void buildRegistry(RegistryBuilder registryBuilder) {
+		registryBuilder.addRegistry(RegistryKeys.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap);
+		registryBuilder.addRegistry(RegistryKeys.PLACED_FEATURE, ModPlacedFeatures::bootstrap);
 	}
 }
--- src/main/java/net/creek/underdark/datagen/ModRegistryDataGenerator.java
+++ src/main/java/net/creek/underdark/datagen/ModRegistryDataGenerator.java
@@ -2,6 +2,7 @@ package net.creek.underdark.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricDynamicRegistryProvider;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryWrapper;
 
 import java.util.concurrent.CompletableFuture;
@@ -12,10 +13,13 @@ public class ModRegistryDataGenerator extends FabricDynamicRegistryProvider {
     }
 
     @Override
-    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup, Entries entries) {
-
+    protected void configure(RegistryWrapper.WrapperLookup registries, Entries entries) {
+        entries.addAll(registries.getWrapperOrThrow(RegistryKeys.CONFIGURED_FEATURE));
+        entries.addAll(registries.getWrapperOrThrow(RegistryKeys.PLACED_FEATURE));
     }
 
     @Override
-    public String getName() { return ""; }
+    public String getName() {
+        return null;
+    }
 }
