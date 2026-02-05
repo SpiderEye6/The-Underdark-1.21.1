8a59a8c1ea3e7c8c6e5f284ce7ca5f7c98be3b04
Norsdu <norsdu@gmail.com>
2026-02-04 23:36:20 -0600

    Fix Underdark portal connection

--- src/main/java/net/creek/underdark/TheUnderdark.java
+++ src/main/java/net/creek/underdark/TheUnderdark.java
@@ -28,7 +28,7 @@ public class TheUnderdark implements ModInitializer {
 		CustomPortalBuilder.beginPortal()
 				.frameBlock(Blocks.REINFORCED_DEEPSLATE)
 				.lightWithItem(Items.OMINOUS_TRIAL_KEY)
-				.destDimID(Identifier.of("the_end"))
+				.destDimID(Identifier.of("underdark:the_underdark"))
 				.tintColor(0,104,168)
 				.onlyLightInOverworld()
 				.forcedSize(20, 6)
